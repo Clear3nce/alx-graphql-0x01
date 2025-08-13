74ee1129db4664b00af492d0e7cb033de85c643e
clearence Muluvhu <clearencemuluvhu@gmail.com>
2025-08-13 14:16:27 +0200

    added rick and morty-app

---
+++ alx-rick-and-morty-app/pages/index.tsx
@@ -0,0 +1,10 @@
+const Home: React.FC = () => {
+  return (
+    <div className="min-h-screen bg-gray-100 flex flex-col justify-center items-center text-center">
+      <h1>RICK AND MORTY APP</h1>
+      <p>graphql</p>
+    </div>
+  );
+};
+
+export default Home;
